12ee372838cfb7ed353e4a8dfb42380c4d42e7ac
Hassan Ali <hassan.ali@g.austincc.edu>
2023-09-25 17:27:51 -0500

    Time: 493 ms (23.55%), Space: 33.5 MB (13.43%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = Counter(nums)
+
+        for val in count.values():
+            if val > 1:
+                return True
+
+        return False
+        
