8df848918b3b3220928abd3c693432da5c1f2bf8
Hassan Ali <hassan.ali@g.austincc.edu>
2023-10-10 11:42:57 -0500

    Time: 114 ms (62.18%), Space: 17.1 MB (45.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,33 +3,34 @@ class Solution:
         
         if len(t) > len(s):
             return ""
-        
         l = 0
-        count_t = Counter(t)
-        window = {}
         
         res = [-1,-1]
         min_len = float("inf")
-        
-        need,have = len(count_t),0
+        count_t = Counter(t)
+        window = {}
+        have,need = 0,len(count_t)
         
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1+window.get(c,0)
             
-            if c in count_t and count_t[c] == window[c]:
+            c = s[r]
+            window[c] = 1 + window.get(c,0)
+            
+            if c in count_t and window[c] == count_t[c]:
                 have += 1
-            while have == need:
+            while have  == need:
                 if r-l+1 < min_len:
-                    min_len = r-l+1
+                    min_len = r - l +1
                     res = [l,r]
+                    
                 window[s[l]] -= 1
-                if s[l] in count_t and count_t[s[l]] > window[s[l]]:
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
                     have -= 1
-                    
+                
+                
                 l += 1
-                    
         l,r = res
         return s[l:r+1]
-        
+            
+            
         
