1d86c3b15a65616a1ca99a8ea461fac6537e62e0
Hassan Ali <hassan.ali@g.austincc.edu>
2023-10-29 22:06:25 -0500

    Time: 97 ms (82.13%), Space: 16.4 MB (25.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,15 +2,14 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
         res = []
-        
         row_len = len(board)
         col_len = len(board[0])
         
         for r in range(row_len):
             for c in range(col_len):
-                elem = board[r][c]
-                if elem.isdigit():
-                    res += [(r,elem),(elem,c),(r//3,c//3,elem)]
-        print(res)         
-        return len(res) == len(list(set(res)))
+                ele = board[r][c]
+                if ele.isdigit():
+                    res += [(ele,r),(c,ele),(r//3,c//3,ele)]
+                    
+        return len(res) == len(set(res))
         
