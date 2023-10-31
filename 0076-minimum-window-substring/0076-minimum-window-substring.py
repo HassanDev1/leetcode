544ab79433fd6567cc6afd7368cb590e9adbf2c5
Hassan Ali <hassan.ali@g.austincc.edu>
2023-10-30 22:50:18 -0500

    Time: 151 ms (34.61%), Space: 16.9 MB (99.60%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,32 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_dict = {}
-        t_dict = {}
-
-        for char in t:
-            t_dict[char] = t_dict.get(char, 0) + 1
-
-        left = 0
-        min_len = float('inf')
-        min_window = ""
+        
+        t_freq = Counter(t)
+        window_s = {}
+        min_len = float("inf")
         count = 0
-
+        min_window = ""
+        
+        left = 0
         for right in range(len(s)):
-            if s[right] in t_dict:
-                s_dict[s[right]] = s_dict.get(s[right], 0) + 1
-                if s_dict[s[right]] <= t_dict[s[right]]:
+            if s[right] in t_freq:
+                window_s[s[right]] = 1 + window_s.get(s[right],0)
+                
+                if window_s[s[right]] <= t_freq[s[right]]:
                     count += 1
-
+                
             while count == len(t):
                 if right - left + 1 < min_len:
-                    min_len = right - left + 1
+                    min_len = right-left + 1
                     min_window = s[left:right+1]
-
-                if s[left] in t_dict:
-                    s_dict[s[left]] -= 1
-                    if s_dict[s[left]] < t_dict[s[left]]:
+                if s[left] in t_freq:
+                    window_s[s[left]] -= 1
+                    if window_s[s[left]] < t_freq[s[left]]:
                         count -= 1
+                    
                 left += 1
-
         return min_window
         
