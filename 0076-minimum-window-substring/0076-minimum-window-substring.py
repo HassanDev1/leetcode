ef4beae3501520ed276df3730a94f7b591e479b6
Hassan Ali <hassan.ali@g.austincc.edu>
2023-10-30 19:08:18 -0500

    Time: 154 ms (33.42%), Space: 17.1 MB (34.69%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,36 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        if len(t) > len(s):
-            return ""
-        l = 0
-        
-        res = [-1,-1]
-        min_len = float("inf")
-        count_t = Counter(t)
-        window = {}
-        have,need = 0,len(count_t)
-        
-        for r in range(len(s)):
-            
-            c = s[r]
-            window[c] = 1 + window.get(c,0)
-            
-            if c in count_t and window[c] == count_t[c]:
-                have += 1
-            while have  == need:
-                if r-l+1 < min_len:
-                    min_len = r - l +1
-                    res = [l,r]
-                    
-                window[s[l]] -= 1
-                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
-                    have -= 1
-                
-                
-                l += 1
-        l,r = res
-        return s[l:r+1]
-            
-            
+        s_dict = {}
+        t_dict = {}
+
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+
+        left = 0
+        min_len = float('inf')
+        min_window = ""
+        count = 0
+
+        for right in range(len(s)):
+            if s[right] in t_dict:
+                s_dict[s[right]] = s_dict.get(s[right], 0) + 1
+                if s_dict[s[right]] <= t_dict[s[right]]:
+                    count += 1
+
+            while count == len(t):
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    min_window = s[left:right+1]
+
+                if s[left] in t_dict:
+                    s_dict[s[left]] -= 1
+                    if s_dict[s[left]] < t_dict[s[left]]:
+                        count -= 1
+                left += 1
+
+        return min_window
         
