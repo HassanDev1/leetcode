c2acf8849b4bad50ac11a0127f13cf67effa7362
Hassan Ali <hassan.ali@g.austincc.edu>
2023-11-07 15:37:28 -0600

    Time: 140 ms (40.19%), Space: 17.1 MB (67.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
         
-        min_window = ""
-        min_len = float("inf")
+        count_t = Counter(t)
+        window = {}
+        have,need = 0,len(t)
         l = 0
-        count = 0
-        
-        t_dict = Counter(t)
-        s_dict = {}
+        min_len = float("inf")
+        res = ""
         
         for r in range(len(s)):
-            if s[r] in t_dict:
-                s_dict[s[r]] = s_dict.get(s[r],0) + 1
-                if s_dict[s[r]] <= t_dict[s[r]]:
-                    count += 1
-            while count == len(t):
-                if r -l + 1 < min_len:
-                    min_len = r - l + 1
-                    min_window = s[l:r+1]
-                #shrink my window
-                if s[l] in t_dict:
-                    s_dict[s[l]] -= 1
-                    if s_dict[s[l]] < t_dict[s[l]]:
-                        count -= 1
+            if s[r] in count_t:
+                window[s[r]] = window.get(s[r],0) +1
+                if window[s[r]] <= count_t[s[r]]:
+                    have += 1
+            while have == need:
+                if r-l+1 < min_len:
+                    min_len = r-l+1
+                    res = s[l:r+1]
+                if s[l] in count_t:
+                    window[s[l]] -= 1
+                    
+                    if window[s[l]] < count_t[s[l]]:
+                        have -= 1
+ 
                 l += 1
-        return min_window
-        
+        return res
+                    
         
