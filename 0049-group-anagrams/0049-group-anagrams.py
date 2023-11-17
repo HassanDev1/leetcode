ceade9051a701619321f4b5388649d36aeb4881b
Hassan Ali <hassan.ali@g.austincc.edu>
2023-11-17 09:45:28 -0600

    Time: 98 ms (64.20%), Space: 19.9 MB (61.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        res = defaultdict(list)
+        hmap = {}
         
-        for ele in strs:
-            sorted_ele = "".join(sorted(ele))
-            res[sorted_ele].append(ele)
-            
-        return res.values()
+        for c in strs:
+            sorted_str = "".join(sorted(c))
+            if sorted_str not in hmap:
+                hmap[sorted_str] = [c]
+            else:
+                hmap[sorted_str].append(c)
+                
+        return hmap.values()
         
