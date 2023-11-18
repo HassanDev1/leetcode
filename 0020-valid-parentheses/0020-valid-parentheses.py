2baaba563389f7c3b8567e4b8cc38165c4bcfcfc
Hassan Ali <hassan.ali@g.austincc.edu>
2023-11-18 11:01:20 -0600

    Time: 42 ms (31.84%), Space: 16.4 MB (18.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        store = {"(":")","[":"]","{":"}"}
-        
-        
         stack = []
         
         for c in s:
             if c in "([{":
                 stack.append(c)
-                
             else:
-                if not stack or store[stack[-1]] != c:
+                if stack:
+                    popped=  stack.pop()
+                    if popped == "(" and c != ")" or popped == "[" and c != "]" or popped == "{" and c != "}":
+                        return False
+                elif not stack and c in ")]}":
                     return False
-                else:
-                    if stack:
-                        stack.pop()
-                        
+                    
         return len(stack) == 0
+                
+        
