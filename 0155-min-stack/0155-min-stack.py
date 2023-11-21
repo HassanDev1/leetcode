8c15e5fdfca99584837039f3b71240e20ecc3b6c
Hassan Ali <hassan.ali@g.austincc.edu>
2023-11-21 11:25:12 -0600

    Time: 64 ms (43.31%), Space: 20.2 MB (77.23%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -8,12 +8,14 @@ class MinStack:
     def push(self, val: int) -> None:
         self.stack.append(val)
         if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)  
+            self.min_stack.append(val)
+        
 
     def pop(self) -> None:
         popped = self.stack.pop()
         if self.min_stack and popped == self.min_stack[-1]:
-            self.min_stack.pop()   
+            self.min_stack.pop()
+        
 
     def top(self) -> int:
         return self.stack[-1]
