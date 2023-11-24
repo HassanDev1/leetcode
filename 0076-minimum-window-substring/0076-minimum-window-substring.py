9fb213f71b34aa77390d97aa6e00f674bfeb8111
Hassan Ali <hassan.ali@g.austincc.edu>
2023-11-24 12:33:54 -0600

    Time: 143 ms (39.12%), Space: 17.1 MB (68.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,36 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
         
         res = ""
-        freq =  Counter(t)
-        window = {}
+        have,need = 0,len(t)
         l = 0
         min_len = float("inf")
-        have = 0
-        need = len(t)
+        window = {}
+        seen = Counter(t)
         
         for r in range(len(s)):
-            if s[r] in freq:
-                window[s[r]] = window.get(s[r],0) + 1
-                if window[s[r]] <= freq[s[r]]:
+            if s[r] in seen:
+                window[s[r]] = window.get(s[r],0)+1
+                
+                if window[s[r]] <= seen[s[r]]:
                     have += 1
             while have == need:
-                if r -l + 1 < min_len:
+                if r -l +1 < min_len:
                     min_len = r-l+1
                     res = s[l:r+1]
-                
-                if s[l] in freq:
+                if s[l] in seen:
                     window[s[l]] -= 1
-                    
-                    if window[s[l]] < freq[s[l]]:
+                    if window[s[l]] < seen[s[l]]:
                         have -= 1
                     
                 l += 1
         return res
-                
-            
-            
-            
-        
