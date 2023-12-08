d7d49db5c4811d17a517373b1d4c55162439aca8
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-07 18:24:41 -0600

    Time: 51 ms (9.82%), Space: 17.2 MB (81.20%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -10,9 +10,10 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
-        oldToCopy = {None:None}
-        curr = head
         
+        oldToCopy = {None:None}
+        
+        curr = head
         while curr:
             copy = Node(curr.val)
             oldToCopy[curr] = copy
@@ -24,7 +25,4 @@ class Solution:
             copy.next = oldToCopy[curr.next]
             copy.random = oldToCopy[curr.random]
             curr = curr.next
-            
         return oldToCopy[head]
-            
-        
