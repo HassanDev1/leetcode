8d0cf2e33541d93468490aab2ae6c6e2d1fc01cb
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-08 11:59:55 -0600

    Time: 706 ms (48.79%), Space: 78.6 MB (20.60%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,29 +2,29 @@ class Node:
     def __init__(self,key,val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.next=self.prev = None
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
-        self.left,self.right = Node(0,0),Node(0,0)
+        self.left = Node(0,0)
+        self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
+        
     def remove(self,node):
-        prev = node.prev
+        prev_node = node.prev
         next_node = node.next
-        prev.next = next_node
-        next_node.prev = prev
+        prev_node.next = next_node
+        next_node.prev = prev_node
     def insert(self,node):
         prev_node = self.right.prev
         prev_node.next = node
         self.right.prev = node
         node.next = self.right
         node.prev = prev_node
-        
-        
-
+    
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
