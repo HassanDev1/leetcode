6b7178eb7300ac5117b0d0b9b714ad60a0ea3f6c
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-11 16:05:04 -0600

    Time: 63 ms (80.38%), Space: 16.4 MB (69.97%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
         
-        seen =  Counter(s1)
+        freq = Counter(s1)
         window = {}
         
+        if len(s1) > len(s2):
+            return False
         l = 0
-        
         for r in range(len(s2)):
             window[s2[r]] = window.get(s2[r],0) + 1
             
-            if r-l+1 == len(s1):
-                if window == seen:
+            if r -l +1 == len(s1):
+                if window == freq:
                     return True
+                if window[s2[l]] == 1:
+                    del window[s2[l]]
                 else:
-                    if window[s2[l]] == 1:
-                        del window[s2[l]]
-                    else:
-                        window[s2[l]] -= 1
+                    window[s2[l]] -= 1
                 l += 1
-                        
+                
         return False
-        
-        
-        
+            
         
