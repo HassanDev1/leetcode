5e915bf13de2e5b8db49523cfa50863e6d886a61
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-14 10:48:16 -0600

    Time: 37 ms (82.01%), Space: 16.8 MB (15.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        res = []
         stack = []
+        res = []
         
-        def backtrack(opened,closed):
-            if opened == closed == n:
+        def backtrack(opened_count,closed_count):
+            if opened_count == closed_count == n:
                 res.append("".join(stack))
                 
-            if opened < n:
+                
+            if opened_count < n:
                 stack.append("(")
-                backtrack(opened+1,closed)
+                backtrack(opened_count+1,closed_count)
                 stack.pop()
-            if closed < opened:
+            if closed_count < opened_count:
                 stack.append(")")
-                backtrack(opened,closed+1)
+                backtrack(opened_count,closed_count+1)
                 stack.pop()
         
         backtrack(0,0)
