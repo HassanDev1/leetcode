2df4bc39609c4bb97119413e5b5bab93cf5769ed
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-17 10:35:03 -0600

    Time: 101 ms (44.38%), Space: 20 MB (52.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -8,18 +8,19 @@ class Solution:
         
         if not lists:
             return
-        
         while len(lists) > 1:
             merged = []
+            
             for i in range(0,len(lists),2):
                 l1 = lists[i]
                 l2 = lists[i+1] if (i+1) < len(lists) else None
+                
                 merged.append(self.merge(l1,l2))
             lists = merged
-            
         return lists[0]
     def merge(self,l1,l2):
-        new_list= curr = ListNode()
+        
+        new_list = curr = ListNode()
         
         while l1 and l2:
             if l1.val < l2.val:
@@ -36,3 +37,4 @@ class Solution:
             curr.next = l2
             
         return new_list.next
+        
