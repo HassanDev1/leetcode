01533556b43979c433aa1f046cd4f83e7beff0d2
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-18 12:23:54 -0600

    Time: 33 ms (88.16%), Space: 16.3 MB (31.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,16 +6,20 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # if not root:
-        #     return []
+        
+        if not root:
+            return []
+        res= []
         stack = []
-        res = []
-        while root or  stack:
+        
+        while root or stack:
             while root:
                 stack.append(root)
                 root = root.left
+                
             root = stack.pop()
             res.append(root.val)
             root = root.right
             
         return res
+        
