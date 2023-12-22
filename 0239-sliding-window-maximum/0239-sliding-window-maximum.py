f3a63f41fa08af0c960652372d35570f835b3750
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-22 12:32:30 -0600

    Time: 1148 ms (99.90%), Space: 32.9 MB (77.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
+        
         q = deque()
         res = []
         
@@ -8,12 +9,13 @@ class Solution:
             
             while q and q[0] < i-k+1:
                 q.popleft()
+                
             while q and nums[q[-1]] < num:
                 q.pop()
                 
             q.append(i)
+            
             if i >= k-1:
                 res.append(nums[q[0]])
                 
         return res
-        
