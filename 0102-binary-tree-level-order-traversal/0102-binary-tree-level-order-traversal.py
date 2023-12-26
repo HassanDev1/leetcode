db5f0a75e835d81950173606cc9ea1e85482c7e8
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-26 15:33:28 -0600

    Time: 43 ms (69.61%), Space: 18.1 MB (17.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -10,20 +10,19 @@ class Solution:
         res = []
         if not root:
             return res
-        
         q = deque([root])
         
         while q:
-            level = []
             size = len(q)
+            level = []
             for _ in range(size):
                 node = q.popleft()
-                
                 level.append(node.val)
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
             res.append(level)
+            
         return res
         
