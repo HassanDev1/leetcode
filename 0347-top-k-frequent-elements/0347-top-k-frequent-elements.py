fb5a0a8250229b063994762d326019efe7fc1819
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-30 09:49:59 -0600

    Time: 111 ms (14.17%), Space: 21.5 MB (41.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,15 +3,13 @@ class Solution:
         heap = []
         
         freq = Counter(nums)
-        
         for key,val in freq.items():
             heappush(heap,(val,key))
+            
             if len(heap) > k:
                 heappop(heap)
                 
         res = []
-        
         for val,key in heap:
             res.append(key)
-            
         return res
