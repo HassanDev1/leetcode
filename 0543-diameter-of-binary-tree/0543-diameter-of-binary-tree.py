d2862125ce4cbd24440200afbec118c463af4e16
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-29 19:48:40 -0600

    Time: 97 ms (7.75%), Space: 18.4 MB (95.39%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -8,14 +8,15 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
         res = [0]
-        def findHeight(node):
+        
+        def dfs(node):
             if not node:
                 return 0
-            lh = findHeight(node.left)
-            rh = findHeight(node.right)
-            res[0] = max(res[0],lh+rh)
-            return 1 + max(lh,rh)
-        findHeight(root)
-        return res[0]
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res[0] = max(res[0],left+right)
             
+            return 1 + max(left,right)
+        dfs(root)
+        return res[0]
         
