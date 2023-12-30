b35d8024e1e25005683baa1b5867930b1bda65ae
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-30 09:46:52 -0600

    Time: 793 ms (7.60%), Space: 74.6 MB (41.37%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -6,18 +6,18 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.store[key].append((value,timestamp))
         
-    
+
     def get(self, key: str, timestamp: int) -> str:
         items = self.store[key]
-        l,r =0,len(items)-1
         res = ""
+        l,r =0,len(items)-1
         while l <= r:
             mid = l + (r-l)//2
             if items[mid][1] <= timestamp:
                 res = items[mid][0]
-                l = mid + 1
+                l = mid+1
             else:
-                r = mid - 1
+                r = mid-1
         return res
         
 
