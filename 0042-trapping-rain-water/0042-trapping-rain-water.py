3980ecb49971cab3753857c270225c572cd7862a
Hassan Ali <hassan.ali@g.austincc.edu>
2023-12-30 21:07:19 -0600

    Time: 226 ms (8.52%), Space: 19.3 MB (8.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,11 +1,13 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-        l = 0
-        r = len(height)-1
+        
+        trap = 0
+
+        l,r = 0,len(height)-1
+        
         max_left = height[0]
         max_right = height[-1]
-        trap = 0
         
         while l < r:
             if height[l] < height[r]:
@@ -18,4 +20,3 @@ class Solution:
                 r -= 1
                 
         return trap
-        
