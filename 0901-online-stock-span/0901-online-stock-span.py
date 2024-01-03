566c1f540a67deab1a39c23562491b6fdf9392c2
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-03 13:02:41 -0600

    Time: 273 ms (98.38%), Space: 22 MB (29.15%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,16 +1,16 @@
 class StockSpanner:
 
     def __init__(self):
-        
         self.stack = []
         
 
     def next(self, price: int) -> int:
         span = 1
-        while self.stack and self.stack[-1][1] <= price:
-            prev_span,prev_price = self.stack.pop()
+        while self.stack and self.stack[-1][0] <= price:
+            _,prev_span = self.stack.pop()
             span += prev_span
-        self.stack.append((span,price))
+            
+        self.stack.append((price,span))
         return span
         
 
