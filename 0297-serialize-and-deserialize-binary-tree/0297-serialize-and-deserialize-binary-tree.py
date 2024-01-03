29a97191c3a5492dd1d98e8d66369ce35ab0d9d7
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-03 13:56:52 -0600

    Time: 90 ms (89.75%), Space: 21.6 MB (83.69%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -33,17 +33,17 @@ class Codec:
         """
         values = data.split(",")
         self.i = 0
-        
         def dfs():
             if values[self.i] == "N":
                 self.i += 1
                 return None
+                
+                
             node = TreeNode(int(values[self.i]))
             self.i += 1
             node.left = dfs()
             node.right = dfs()
             return node
-    
         return dfs()
             
         
