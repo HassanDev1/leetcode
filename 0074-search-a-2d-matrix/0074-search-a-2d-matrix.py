c3337fb56f042579738516685e74e429feacb247
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-03 10:22:20 -0600

    Time: 53 ms (39.27%), Space: 17.7 MB (18.40%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,16 +3,15 @@ class Solution:
         
         
         for rows in matrix:
-            l,r =0,len(rows)-1
-            
+            l,r = 0,len(rows)-1
             while l <= r:
                 mid = l + (r-l)//2
                 if rows[mid] == target:
                     return True
                 elif target > rows[mid]:
-                    l = mid + 1
+                    l  = mid + 1
                 else:
-                    r = mid-1
+                    r = mid -1
                     
         return False
         
