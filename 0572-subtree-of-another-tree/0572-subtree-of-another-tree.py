31700f1a8b6297c6e4182080fb5ad409de89b124
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-05 10:11:14 -0600

    Time: 125 ms (14.87%), Space: 17.5 MB (78.72%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,11 +8,23 @@ class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
         def compare(p,q):
-            if not p and not q:
-                return True
-            if not p and q or p and not q or p.val != q.val:
+            if p.val != q.val:
                 return False
-            return compare(p.left,q.left) and compare(p.right,q.right)
+            q = deque([(p,q)])
+            while q:
+                size = len(q)
+
+                for _ in range(size):
+                    n1,n2 = q.popleft()
+                    if not n1 and n2 or n1 and not n2:
+                        return False
+                    elif n1 and n2 and n1.val != n2.val:
+                        return False
+                    if n1 and n2:
+                        q.append((n1.left,n2.left))
+                        q.append((n1.right,n2.right))
+            return True
+            
         
         q = deque([root])
         
