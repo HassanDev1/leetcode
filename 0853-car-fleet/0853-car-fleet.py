02fe057cd790e2f87b47209811a4bee5c8241a88
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-09 10:45:03 -0600

    Time: 688 ms (91.21%), Space: 41.9 MB (7.78%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -6,11 +6,8 @@ class Solution:
         fleets.sort(key=lambda x:x[0],reverse=True)
         
         stack = []
-        
         for p,t in fleets:
-            if not stack or stack[-1] < t:
+            while not stack or stack[-1] < t:
                 stack.append(t)
                 
-                
         return len(stack)
-        
