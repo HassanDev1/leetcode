24f261664e84fb0ef14a8dbc0a49fa80cebaf852
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-11 17:33:36 -0600

    Time: 438 ms (41.20%), Space: 38.4 MB (21.42%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -5,21 +5,18 @@ class Solution:
         
         count = Counter(arr)
         
-        deletion = 0
-        heap = []
+        
         
         for m,v in count.items():
             heappush(heap,(v,m))
             
-        for i in range(len(heap)):
+        deletion = 0
+            
+        for _ in range(len(heap)):
             deletion += heap[0][0]
             if deletion > k:
                 break
             heappop(heap)
+            
                 
         return len(heap)
-            
-            
-            
-            
-            
