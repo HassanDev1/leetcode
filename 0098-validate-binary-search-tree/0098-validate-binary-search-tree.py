3cc75da892547f805e3c8200e8a057bd3eb665b0
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-11 10:39:22 -0600

    Time: 38 ms (92.92%), Space: 19.3 MB (28.50%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,18 +9,19 @@ class Solution:
         
         stack = []
         prev = float("-inf")
+        
         while root or stack:
-            
             while root:
                 stack.append(root)
                 root = root.left
+                
             root = stack.pop()
-            
             if prev >= root.val:
                 return False
             prev = root.val
             root = root.right
             
         return True
-           
+                
+            
         
