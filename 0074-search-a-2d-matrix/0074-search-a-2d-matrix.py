9e26aefebf292162e1bc1e66a459779ce7231d9a
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-22 12:59:22 -0600

    Time: 36 ms (98.36%), Space: 17.1 MB (58.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,7 +4,6 @@ class Solution:
         
         for row in matrix:
             l,r =0,len(row)-1
-            
             while l <= r:
                 mid = l + (r-l)//2
                 if row[mid] == target:
@@ -12,6 +11,8 @@ class Solution:
                 elif target > row[mid]:
                     l = mid + 1
                 else:
-                    r = mid - 1
+                    r = mid-1
                     
         return False
+                    
+                
