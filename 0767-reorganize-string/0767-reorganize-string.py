08ca34946345b9eb080748c62ee76478d84662d3
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-30 10:51:11 -0600

    Time: 23 ms (99.76%), Space: 16.7 MB (58.79%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,28 +1,27 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         
-        freq = Counter(s)
+        
+        freq =  Counter(s)
         heap = []
         for char,count in freq.items():
-            if count != 0:
-                heappush(heap,(-count,char))
-                
-        prev_freq,prev_char = 0,""
+            heappush(heap,(-count,char))
+            
         res = []
+        prev_count,prev_char = 0,""
         while heap:
             count,char = heappop(heap)
             
             res.append(char)
-            if prev_freq < 0:
-                heappush(heap,(prev_freq,prev_char))
-            
+            if prev_count < 0:
+                heappush(heap,(prev_count,prev_char))
+                
             count += 1
-            prev_freq = count
+            prev_count = count
             prev_char = char
             
         if len(res) != len(s):
             return ""
         return "".join(res)
-        
             
-        
+            
