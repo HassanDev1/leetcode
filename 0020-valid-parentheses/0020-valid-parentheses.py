40ea32d7e4950270125afeee17c1a332e6b46568
Hassan Ali <hassan.ali@g.austincc.edu>
2024-01-31 12:42:29 -0600

    Time: 38 ms (54.43%), Space: 16.6 MB (68.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,16 +4,14 @@ class Solution:
         stack = []
         
         for c in s:
-            if c in "([{":
+            if c in "({[":
                 stack.append(c)
-                
-            elif not stack and c in ")]}":
-                return False
-            else:
-                if stack and stack[-1] == "(" and c != ")" or stack[-1] == "{" and c != "}" or stack[-1] == "[" and c != "]":
+            elif not stack and c in ")}]":
                     return False
-                
+            else:
+                if stack and stack[-1] == "(" and c != ")" or stack[-1] == "[" and c != "]" or stack[-1] == "{" and c != "}":
+                    return False
                 stack.pop()
-                
+            
         return not stack
         
