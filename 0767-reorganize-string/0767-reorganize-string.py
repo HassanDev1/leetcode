b9929ac5a5d3e5e34c4dd5f5e2f5517bf0f5b462
Hassan Ali <hassan.ali@g.austincc.edu>
2024-02-09 21:53:18 -0600

    Time: 38 ms (63.44%), Space: 16.5 MB (80.88%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,23 +3,27 @@ class Solution:
         
         freq = Counter(s)
         heap = []
+        
         for char,count in freq.items():
-            heappush(heap,(-count,char))
+            if count:
+                heappush(heap,(-count,char))
             
+        prev_count = 0
+        prev_char = ""
         res = []
-        prev_char,prev_count = "",0
         while heap:
             count,char = heappop(heap)
-            res.append(char)
             
+            res.append(char)
             if prev_count < 0:
                 heappush(heap,(prev_count,prev_char))
+                
             count += 1
-        
+            
             prev_count = count
             prev_char = char
-                
-                
+            
         if len(res) != len(s):
             return ""
         return "".join(res)
+        
