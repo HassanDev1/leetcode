5d6239e1b0ec8c911be65d3854559cf30fc901e2
Hassan Ali <hassan.ali@g.austincc.edu>
2024-02-10 16:32:35 -0600

    Time: 41 ms (32.87%), Space: 16.6 MB (67.13%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -3,16 +3,15 @@ class Solution:
         
         
         heap = []
+        
         for s in stones:
             heappush(heap,-s)
             
         while len(heap) > 1:
             x = heappop(heap)
             y = heappop(heap)
-            
             if x != y:
-                new_weight = abs(x-y)
-                heappush(heap,-new_weight)
+                new_s = abs(x-y)
+                heappush(heap,-new_s)
                 
         return -heap[0] if heap else 0
-                
