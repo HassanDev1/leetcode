eb7751c37bb96a810e7beb6b446f9c92707c79ee
Hassan Ali <hassan.ali@g.austincc.edu>
2024-02-18 12:03:14 -0600

    Time: 40 ms (45.01%), Space: 16.7 MB (53.22%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        balon_count = {"b":0,"a":0,"l":0,"o":0,"n":0}
-       
+        
+        
+        freq = {"b":0,"l":0,"o":0,"n":0}
+        
         for t in text:
-            if t in balon_count:
-                balon_count[t] += 1
+            if t in freq:
+                freq[t] += 1
                 
-                
-        balon_count['l'] //= 2
-        balon_count['o'] //= 2
+        freq["l"] //= 2
+        freq["o"] //= 2
         
-        return min(balon_count.values())
-        
-        
+        return min(freq.values())
