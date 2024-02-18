bbf622fb0f4ca7d8f5d6d73998f21568325dd0c4
Hassan Ali <hassan.ali@g.austincc.edu>
2024-02-18 12:12:58 -0600

    Time: 40 ms (73.12%), Space: 16.6 MB (63.66%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -6,14 +6,14 @@ class Solution:
             res = [0]*len(code)
             return res
         
-        
-        for i in range(len(code)):
+        for i in range (len(code)):
             if k > 0:
                 m = k
                 j = i+1
                 curr_sum = 0
                 while m:
                     curr_sum += code[j%len(code)]
+                   
                     j += 1
                     m -= 1
                 res.append(curr_sum)
@@ -21,12 +21,14 @@ class Solution:
                 m = k
                 j = i-1
                 curr_sum = 0
-                
                 while m:
                     curr_sum += code[j%len(code)]
+                    
                     j -= 1
                     m += 1
                 res.append(curr_sum)
-        return res
+                    
                 
+        return res
+                    
         
