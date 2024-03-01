70d566ea9d95c5d64e1d24435d27bcfdc61a6ed7
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-01 11:33:49 -0600

    Time: 1792 ms (64.14%), Space: 306.4 MB (54.54%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -2,38 +2,46 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.prefix_count = 0
+        
 class Trie:
     def __init__(self):
         self.root = TrieNode()
         
-    def add_word(self,word):
+    def addWord(self,word):
         curr = self.root
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]
             curr.prefix_count += 1
-    
-    def getScore(self,word):
+            
+            
+    def return_score(self,word):
         curr = self.root
         score = 0
-        
         for c in word:
             curr = curr.children[c]
             score += curr.prefix_count
             
         return score
             
+        
+            
+    
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         
         trie = Trie()
         
         for word in words:
-            trie.add_word(word)
+            trie.addWord(word)
             
         res = []
+        
         for word in words:
-            res.append(trie.getScore(word))
+            res.append(trie.return_score(word))
             
         return res
+            
+        
+            
