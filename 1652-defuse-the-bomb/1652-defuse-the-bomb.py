e21d23c1725fe9f3a53970e22d86c898aefe1342
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-04 17:43:27 -0600

    Time: 50 ms (20.89%), Space: 16.6 MB (53.26%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,31 +1,28 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         
-        
-        res = []
         if k == 0:
-            res = [0]*len(code)
+            res=[0]*len(code)
             return res
-        
-        for i in range(len(code)):  #O(n)
+        res = []
+        for i in range(len(code)):
             if k > 0:
                 m = k
                 j = i+1
                 curr_sum = 0
-                
                 while m:
                     curr_sum += code[j%len(code)]
                     j += 1
-                    m -= 1
+                    m-=1
                 res.append(curr_sum)
-            else:
+            if k < 0:
                 m = k
                 j = i-1
                 curr_sum = 0
-                
                 while m:
                     curr_sum += code[j%len(code)]
                     j -= 1
-                    m += 1
+                    m +=1
                 res.append(curr_sum)
+                
         return res
