4df22551d098fc1a079f36662ce2c8942acf4698
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-05 20:06:04 -0600

    Time: 78 ms (41.80%), Space: 20 MB (51.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,6 +1,5 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
         left = []
         right = []
         
@@ -9,11 +8,9 @@ class Solution:
                 left.append(interval)
             elif interval[0] > newInterval[1]:
                 right.append(interval)
-                
             else:
-                newInterval[0] = min(newInterval[0],interval[0])
                 newInterval[1] = max(newInterval[1],interval[1])
+                newInterval[0] = min(newInterval[0],interval[0])
                 
-        return left + [newInterval]+right
+        return left+[newInterval]+right
             
-        
