e441ed63d1bc304f4501a3c8122e311ed1ad61a3
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-17 12:07:57 -0500

    Time: 38 ms (39.39%), Space: 16.6 MB (77.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
+        freq = {"(":")","[":"]","{":"}"}
+        
         stack = []
+        
         for c in s:
             if c in "([{":
                 stack.append(c)
-                
-            elif  not stack and c in ")]}":
+            elif not stack and c in ")]}":
                 return False
-            
             else:
-                if stack and stack[-1] != "(" and c == ")" or stack[-1] != "[" and c == "]" or  stack[-1] != "{" and c == "}":
+                if stack and stack[-1] == "(" and c != ")" or stack[-1] == "[" and c != "]" or stack[-1] == "{" and c != "}" :
                     return False
-               
+                
                 stack.pop()
-            
         return not stack
