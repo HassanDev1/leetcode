c38b19c910b0898b6924748cf0fc8224b37e32d8
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-21 15:16:46 -0500

    Time: 79 ms (6.38%), Space: 16.9 MB (16.29%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,22 +1,19 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        n = len(s)
-        ans = [float('inf')] * n
-        pos = float('-inf')
-
-        # Iterate from left to right to find closest occurrence of c
-        for i in range(n):
+        
+        res = [len(s)]*len(s)
+        
+        def dfs(i,distance):
+            if  i < 0 or i > len(s)-1 or res[i] <= distance:
+                return
+           
+            res[i] = distance
+            
+            dfs(i+1,distance+1)
+            dfs(i-1,distance+1)
+            
+        for i in range(len(s)):
             if s[i] == c:
-                pos = i
-            ans[i] = min(ans[i], abs(i - pos))
-
-        pos = float('inf')
-
-        # Iterate from right to left to find closest occurrence of c
-        for i in range(n - 1, -1, -1):
-            if s[i] == c:
-                pos = i
-            ans[i] = min(ans[i], abs(i - pos))
-
-        return ans
+                dfs(i,0)
+        return res
         
