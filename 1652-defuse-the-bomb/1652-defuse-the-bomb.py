4f1247e94c5757abdff101af79fb77e99925dc04
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-26 17:05:36 -0500

    Time: 44 ms (50.06%), Space: 16.7 MB (32.04%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,10 +1,9 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        
         if k == 0:
-            res=[0]*len(code)
+            res = [0]*len(code)
             return res
-        res = []
+        res = [0]*len(code)
         for i in range(len(code)):
             if k > 0:
                 m = k
@@ -12,17 +11,16 @@ class Solution:
                 curr_sum = 0
                 while m:
                     curr_sum += code[j%len(code)]
-                    j += 1
+                    res[i] = curr_sum
                     m-=1
-                res.append(curr_sum)
-            if k < 0:
+                    j += 1
+            else:
                 m = k
                 j = i-1
                 curr_sum = 0
                 while m:
                     curr_sum += code[j%len(code)]
+                    res[i] = curr_sum
+                    m += 1
                     j -= 1
-                    m +=1
-                res.append(curr_sum)
-                
         return res
