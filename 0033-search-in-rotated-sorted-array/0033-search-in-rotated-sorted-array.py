9a13678ea262aa3ca3540b1384ae0070e3f62716
Hassan Ali <hassan.ali@g.austincc.edu>
2024-03-29 11:03:47 -0500

    Time: 39 ms (81.45%), Space: 16.9 MB (55.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -9,15 +9,16 @@ class Solution:
             
             if nums[mid] == target:
                 return mid
+            
             elif nums[mid] > nums[r]:
-                if target > nums[mid] or target < nums[l] :
-                    l = mid +1
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
                 else:
-                    r = mid -1
+                    r = mid - 1
             else:
                 if target < nums[mid] or target > nums[r]:
-                    r = mid-1
+                    r = mid - 1
                 else:
                     l = mid + 1
-                    
+                
         return -1
