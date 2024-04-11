2b9f92aca04c8b97c2f4c9acfe96dc6d9aac824b
Hassan Ali <hassan.ali@g.austincc.edu>
2024-04-10 21:32:15 -0500

    Time: 42 ms (30.74%), Space: 16.4 MB (96.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,27 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        def helper(top_left, bottom_right):
+            # Basecase: Single square 
+            if bottom_right <= top_left:
+                return
+
+            # Rotate the items from upper-row with left-column, left-column with bottom-row, bottom-row with right-column, and right-column with upper-row
+            for i in range(top_left, bottom_right):
+                tmp = matrix[top_left][i] # Store upper-row
+                matrix[top_left][i] = matrix[n-i][top_left] # Set upper-row with value in left-column
+                matrix[n-i][top_left] = matrix[n-top_left][n-i] # Set left-column with value in bottom-row
+                matrix[n-top_left][n-i] = matrix[n-(n-i)][n-top_left] # Set bottom-row with value in right-column
+                matrix[n-(n-i)][n-top_left] = tmp # Set the right-column with upper-row
+
+            # Recursive reduce the layers
+            helper(top_left+1, bottom_right-1)
         
+        # Run helper function starting at the outermost layer
+        n = len(matrix)-1
+        helper(0, n)
         
-        
-        l,r = 0,len(matrix)-1
-        while l < r:
-            matrix[l],matrix[r] = matrix[r],matrix[l]
-            l += 1
-            r -= 1
-        res = []
-        for c in range(len(matrix[0])):
-            temp = []
-            for r in range(len(matrix)):
-                temp.append(matrix[r][c])
-            res.append(temp)
-            
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                matrix[r][c] = res[r][c]
-                
-            
-        
-        print(matrix)
-            
-            
