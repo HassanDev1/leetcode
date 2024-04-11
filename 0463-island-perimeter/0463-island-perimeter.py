4a90105c19a1c21ff38ee6a258da13ea346b477d
Hassan Ali <hassan.ali@g.austincc.edu>
2024-04-11 13:08:42 -0500

    Time: 360 ms (75.70%), Space: 16.9 MB (92.38%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,16 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] ==  1:
+                    perimeter += 4
+                    
+                    if col > 0 and grid[row][col-1] == 1:
+                        perimeter -= 2
+                    if row > 0 and grid[row-1][col] == 1:
+                        perimeter -= 2
+                        
+        return perimeter
+        
