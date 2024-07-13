f9698147a0a9392c80bec5339548c648e6a205b3
Hassan Ali <hassan.ali@g.austincc.edu>
2024-07-13 14:02:02 -0500

    Time: 1284 ms (18.68%), Space: 39.8 MB (7.50%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -2,21 +2,21 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
         res = []
-        curr_max = max(nums[:k])
-        l = 0
         heap = []
         for i in range(0,k):
             heappush(heap,(-nums[i],i))
             
         res.append(heap[0][0])
+        
         for i in range(k,len(nums)):
+            
             
             while heap and heap[0][1] <= i-k:
                 heappop(heap)
-            
             heappush(heap,(-nums[i],i))
-            res.append(heap[0][0])
             
+            res.append(heap[0][0])
+
        
         for i in range(len(res)):
             res[i] = -1*res[i]
