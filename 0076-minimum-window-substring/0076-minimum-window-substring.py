a3dcd5d0963e3f053a26fd4bf4c11c6a97aa281c
Hassan Ali <hassan.ali@g.austincc.edu>
2024-07-12 19:42:42 -0500

    Time: 120 ms (55.56%), Space: 17.3 MB (19.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
         
-        min_len = float("inf")
-        l = 0
+        s_freq = {}
         window = Counter(t)
+        l = 0
+        have,need =0,len(window)
+        min_len = float("inf")
+        res = [-1,-1]
         
-        have = 0
-        need = len(t)
-        freq = {}
-        res = ""
-        
-        for r in range(len(s)) :
-            if s[r] in window:
-                freq[s[r]] = freq.get(s[r],0) + 1
-                if freq[s[r]] <= window[s[r]]:
-                    have += 1
+        for r in range(len(s)):
+            s_freq[s[r]] = s_freq.get(s[r],0) + 1
+            
+            if s[r] in window and s_freq[s[r]] == window[s[r]]:
+                have += 1
+                
             while have == need:
-                if r-l+1 < min_len:
+                if (r-l+1) < min_len:
+                    res = [l,r]
                     min_len = r-l+1
-                    res = s[l:r+1]
-                if s[l] in window:
-                    freq[s[l]] -= 1
-                    if freq[s[l]] < window[s[l]]:
-                        have -= 1
-                        
+                    
+                s_freq[s[l]] -= 1
+                if s[l] in window and s_freq[s[l]] < window[s[l]]:
+                    have -= 1
+                    
                 l += 1
-        return res
+                
+        l,r = res
+        return s[l:r+1] if min_len != float("inf") else ""
         
