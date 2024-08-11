1f3e966ff4b6f4ca41bb1c5a1fdb1839cc040b70
Hassan Ali <hassan.ali@g.austincc.edu>
2024-08-11 17:26:20 -0500

    Time: 259 ms (74.80%), Space: 18.2 MB (46.91%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,25 +3,21 @@ class Solution:
         
         def feasible(mid):
             total = 0
-            for i in range(len(piles)):
-                total += math.ceil(piles[i]/mid)
+            
+            for pile in piles:
+                total += math.ceil(pile/mid)
             if total <= h:
                 return True
             return False
-            
-        l=1
-        r = max(piles)
+        
+        l,r=1,max(piles)
         
         while l < r:
             mid = l + (r-l)//2
-            
             if feasible(mid):
                 r = mid
             else:
-                l = mid + 1
+                l = mid +1
                 
-        if feasible(l) <= h:
-            return l
-        else:
-            return r
-                
+        return l
+        
