72827e42f78bdf8378a1d875404b79036ec8cf38
Hassan Ali <hassan.ali@g.austincc.edu>
2024-09-02 13:26:09 -0500

    Time: 1193 ms (13.44%), Space: 45.3 MB (8.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        cache = {}
-
-        def aux(amount):
-
+        memo = {}
+        def helper(amount):
             if amount == 0:
                 return 0
-
             if amount < 0:
-                return float('inf')
-
-            if amount in cache:
-                return cache[amount]
-
-            cache[amount] = min([1+aux(amount-c) for c in coins])
-            return cache[amount]
-
-        ans = aux(amount)
-        if ans == float('inf'):
+                return float("inf")
+            if amount in memo:
+                return memo[amount]
+            min_coins = float("inf")
+            for coin in coins:
+                ans = helper(amount-coin)+1
+                min_coins = min(ans,min_coins)
+                
+            memo[amount] = min_coins
+            return min_coins
+        res = helper(amount)
+        if res == float("inf"):
             return -1
-        return ans
+        return res
+                
+            
+        
